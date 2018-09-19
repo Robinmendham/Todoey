efaf22a1051d6f66a3852edc3176ff4bc07d6b90
Robin Mendham <robinmendham@me.com>
2018-09-19 10:10:49 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  Todoey
+//
+//  Created by Robin Mendham on 18/09/2018.
+//  Copyright © 2018 Robin Mendham. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
+
